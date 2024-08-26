62a153875e8e5457950f4dd507707a6c1a01dbfe
Muzi09 <muzammil.khan@creysto.com>
2024-08-26 16:48:44 +0530

    revert bg

--- src/Components/About/About.jsx
+++ src/Components/About/About.jsx
@@ -1,5 +1,4 @@
 import React from 'react'
-import Waves from '../Wave/Wave'
 
 function About() {
   return (
--- src/Layout/Layout.jsx
+++ src/Layout/Layout.jsx
@@ -1,12 +1,10 @@
 import React from "react";
 import Navbar from "../Components/Navbar";
 import Footer from "../Components/Footer";
-import Waves from "../Components/Wave/Wave";
 
 const Layout = (props) => {
   return (
     <>
-    <Waves/>
       <Navbar />
         {props.children}
       <Footer />
